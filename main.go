9fbef3482e02b2904bb25b99c8a81dc1d19c46cc
marcelobiao <biao.marcelo@gmail.com>
2023-08-12 12:14:31 -0300

    feat: Criando mais métricas

--- main.go
+++ main.go
@@ -17,15 +17,40 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_http_request_total",
+	Help: "Cout of all Http requests for goapp",
+}, []string{})
+
+var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "goapp_http_request_duration",
+	Help: "Duration in seconds of all HTTP requests",
+}, []string{"handler"})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
+	r.MustRegister(httpRequestTotal)
+	r.MustRegister(httpDuration)
 
 	go func() {
 		for {
 			onlineUsers.Set(float64(rand.Intn(2000)))
 		}
 	}()
+
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello Full Cycle"))
+	})
+
+	d := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "home"}),
+		promhttp.InstrumentHandlerCounter(httpRequestTotal, home),
+	)
+
+	http.Handle("/", d)
+
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
 }
